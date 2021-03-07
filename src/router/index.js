86ecc82492e8546ab5818be48b7e3d01946d7dc0
Przemek Kaliś <przemek.kalis@gmail.com>
2021-03-07 12:18:48 +0100

    rebuild routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,23 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "overview",
+    component: () => import("../views/Overview.vue")
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/hours",
+    name: "hours",
+    component: () => import("../views/Hours.vue")
   }
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  // mode: "history",
   base: process.env.BASE_URL,
   routes
 });
