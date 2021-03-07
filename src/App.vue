d8a921340b319b5a5fdc6e07fd24e0f22ad4e2e1
Przemek Kaliś <przemek.kalis@gmail.com>
2021-03-07 12:18:29 +0100

    rebuild layout

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,35 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <div id="app">
+    <v-app>
+      <v-app-bar app dense>
+        <v-app-bar-title
+          >SYTO<span class="blue--text text--darken-2"
+            >Panel</span
+          ></v-app-bar-title
+        >
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+        <v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-btn exact text :to="{ name: 'overview' }">
+            Przegląd
+          </v-btn>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+          <v-btn exact text :to="{ name: 'hours' }">
+            Godziny
+          </v-btn>
 
-    <v-main>
-      <HelloWorld />
-    </v-main>
-  </v-app>
+          <v-btn icon>
+            <v-icon>mdi-logout</v-icon>
+          </v-btn>
+        </v-toolbar-items>
+      </v-app-bar>
+
+      <v-main>
+        <router-view />
+      </v-main>
+
+      <v-footer app> </v-footer>
+    </v-app>
+  </div>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld";
-
-export default {
-  name: "App",
-
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
-};
-</script>
