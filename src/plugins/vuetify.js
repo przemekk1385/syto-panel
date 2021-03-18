da5b2bbd656017c40f2a2bafaab2ee958b989d46
Przemek Kaliś <przemek.kalis@gmail.com>
2021-03-18 20:04:19 +0100

    add i18n support

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,29 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
+import VueI18n from "vue-i18n";
+
+import { en, pl } from "vuetify/lib/locale";
 
 Vue.use(Vuetify);
+Vue.use(VueI18n);
 
-export default new Vuetify({});
+const messages = {
+  en: {
+    $vuetify: en
+  },
+  pl: {
+    $vuetify: pl
+  }
+};
+
+const i18n = new VueI18n({
+  locale: "pl",
+  fallbackLocale: "en",
+  messages
+});
+
+export default new Vuetify({
+  lang: {
+    t: (key, ...params) => i18n.t(key, params)
+  }
+});
