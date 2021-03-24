d9643696e39eaa6176df08a02e696886a5201dbe
Przemek Kaliś <przemek.kalis@gmail.com>
2021-03-24 20:30:01 +0100

    add stuff for building / serving within Django

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,24 @@
-module.exports = {
-  transpileDependencies: ["vuetify"],
+const path = require("path");
 
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const outputDir = process.env.OUTPUT_DIR || "dist";
+const publicPath = process.env.PUBLIC_PATH || "/";
+
+module.exports = {
+  chainWebpack: config => {
+    config.plugins.delete("html");
+    config.plugins.delete("preload");
+    config.plugins.delete("prefetch");
+  },
+  configureWebpack: {
+    devServer: {
+      writeToDisk: true
+    },
+    plugins: [new CleanWebpackPlugin(), new WebpackManifestPlugin()]
+  },
+  outputDir: path.resolve(__dirname, outputDir),
   pluginOptions: {
     i18n: {
       locale: "pl",
@@ -8,5 +26,7 @@ module.exports = {
       localeDir: "locales",
       enableInSFC: true
     }
-  }
+  },
+  publicPath,
+  transpileDependencies: ["vuetify"]
 };
