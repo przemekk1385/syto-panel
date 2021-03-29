a5086bc8e38b7d9ae8f8718aa6664f871de240ca
Przemek Kaliś <przemek.kalis@gmail.com>
2021-03-29 23:02:38 +0200

    integrate with API

--- src/views/admin/Slots.vue
+++ src/views/admin/Slots.vue
@@ -2,34 +2,152 @@
   <v-container>
     <v-row>
       <v-col>
-        <h1 class="text-h3">Godziny</h1>
+        <h1 class="text-h3">Dni robocze</h1>
       </v-col>
     </v-row>
+
     <v-row>
       <v-col>
-        <v-sheet>
-          <v-toolbar dense flat>
-            <v-spacer></v-spacer>
-            <v-btn icon class="mr-2" @click="$refs.calendar.prev()">
-              <v-icon>mdi-chevron-left</v-icon>
-            </v-btn>
-            <v-btn icon class="ml-2" @click="$refs.calendar.next()">
-              <v-icon>mdi-chevron-right</v-icon>
-            </v-btn>
-          </v-toolbar>
-        </v-sheet>
+        <v-toolbar flat tag="div">
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" width="auto ">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="success"
+                dark
+                fab
+                class="mx-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon dark>mdi-calendar-plus</v-icon>
+              </v-btn>
+            </template>
+            <v-form v-if="dialog" ref="slotForm">
+              <v-card>
+                <v-card-title class="headline">
+                  Dzień roboczy
+                </v-card-title>
+                <v-card-text>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-menu
+                        v-model="datePicker"
+                        :close-on-content-click="false"
+                        :nudge-right="40"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="day"
+                            label="Data"
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            :rules="[rules.required]"
+                            v-bind="attrs"
+                            v-on="on"
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker
+                          v-model="day"
+                          first-day-of-week="1"
+                          landscape
+                          locale="pl"
+                          :min="tomorrow"
+                          :show-current="today"
+                          @change="fillSlotForm(day)"
+                          @input="datePicker = false"
+                        ></v-date-picker>
+                      </v-menu>
+                      <v-text-field
+                        v-model="stationaryWorkersLimit"
+                        label="Limit pracowników stacjonarnych"
+                        prepend-icon="mdi-account-hard-hat"
+                        :rules="[rules.positiveInt]"
+                        @blur="fixStationaryWorkersLimit"
+                      ></v-text-field>
+                      <v-switch
+                        v-model="isOpenForCottageWorkers"
+                        label="Otwarty dla chałupników"
+                      ></v-switch>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" icon outlined @click="submitSlotForm">
+                    <v-icon class="mx-2">
+                      mdi-send
+                    </v-icon>
+                  </v-btn>
+                  <v-btn color="warning" icon outlined @click="resetSlotForm">
+                    <v-icon class="mx-2">
+                      mdi-eraser
+                    </v-icon>
+                  </v-btn>
+                  <v-btn
+                    v-if="id"
+                    color="error"
+                    icon
+                    outlined
+                    @click="deleteSlot"
+                  >
+                    <v-icon class="mx-2">
+                      mdi-trash-can-outline
+                    </v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-form>
+          </v-dialog>
+          <v-btn
+            color="primary"
+            dark
+            fab
+            class="mx-2"
+            @click="$refs.calendar.prev()"
+          >
+            <v-icon dark>mdi-chevron-left</v-icon>
+          </v-btn>
+          <v-btn
+            color="primary"
+            dark
+            fab
+            class="mx-2"
+            @click="$refs.calendar.next()"
+          >
+            <v-icon dark>mdi-chevron-right</v-icon>
+          </v-btn>
+        </v-toolbar>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
         <v-sheet>
           <v-calendar
             ref="calendar"
             v-model="value"
             color="primary"
             locale="pl"
-            :now="today"
             show-week
-            :value="today"
             :weekdays="[1, 2, 3, 4, 5, 6, 0]"
-            @click:date="fillSlotForm"
           >
+            <template v-slot:day-label="{ date, past, present }">
+              <v-btn
+                :color="present ? 'primary' : ''"
+                :disabled="past"
+                elevation="0"
+                fab
+                small
+                :text="!present"
+                @click="fillSlotForm(date)"
+              >
+                {{ formatDate(date) }}
+              </v-btn>
+            </template>
             <template v-slot:day="{ date, past }">
               <v-container v-if="slots[date]" class="mt-1">
                 <v-toolbar dense flat>
@@ -56,184 +174,107 @@
         </v-sheet>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col class="text-right">
-        <v-dialog v-model="dialog" width="auto ">
-          <v-form v-if="dialog" ref="slotForm">
-            <v-card>
-              <v-card-title class="headline">
-                Dzień roboczy
-              </v-card-title>
-              <v-card-text>
-                <v-row>
-                  <v-col cols="12">
-                    <v-menu
-                      v-model="datePicker"
-                      :close-on-content-click="false"
-                      :nudge-right="40"
-                      transition="scale-transition"
-                      offset-y
-                      min-width="auto"
-                    >
-                      <template v-slot:activator="{ on, attrs }">
-                        <v-text-field
-                          v-model="date"
-                          label="Data"
-                          prepend-icon="mdi-calendar"
-                          readonly
-                          :rules="[rules.required]"
-                          v-bind="attrs"
-                          v-on="on"
-                        ></v-text-field>
-                      </template>
-                      <v-date-picker
-                        v-model="date"
-                        first-day-of-week="1"
-                        landscape
-                        locale="pl"
-                        :min="$_calendar_tomorrow"
-                        :show-current="today"
-                        @input="datePicker = false"
-                      ></v-date-picker>
-                    </v-menu>
-                    <v-text-field
-                      v-model="stationaryWorkersLimit"
-                      label="Limit pracowników stacjonarnych"
-                      prepend-icon="mdi-account-hard-hat"
-                      :rules="[rules.positiveInt]"
-                      @blur="fixStationaryWorkersLimit"
-                    ></v-text-field>
-                    <v-switch
-                      v-model="isOpenForCottageWorkers"
-                      label="Otwarty dla chałupników"
-                    ></v-switch>
-                  </v-col>
-                </v-row>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn
-                  aria-label="Send form"
-                  color="primary"
-                  icon
-                  @click="submitSlotForm()"
-                >
-                  <v-icon class="mr-1">
-                    mdi-send
-                  </v-icon>
-                </v-btn>
-                <v-btn
-                  aria-label="Reset form"
-                  color="error"
-                  icon
-                  @click="$refs.slotForm.reset()"
-                >
-                  <v-icon class="mr-1">
-                    mdi-eraser
-                  </v-icon>
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-form>
-        </v-dialog>
-      </v-col>
-    </v-row>
   </v-container>
 </template>
 
 <script>
+require("dayjs/locale/pl");
+
+import { mapActions } from "vuex";
+
 var dayjs = require("dayjs");
 var duration = require("dayjs/plugin/duration");
-
-require("dayjs/locale/pl");
 
 dayjs.locale("pl");
 dayjs.extend(duration);
 
 export default {
-  name: "Calendar",
+  name: "Slots",
   data: () => ({
-    // form stuff
+    slots: {},
+
     dialog: false,
+
     datePicker: false,
 
     rules: {
       positiveInt: value =>
         !value ||
         Number.parseInt(value) > 0 ||
-        "Wymagana liczba całkowita, większa od 0!",
-      required: value => !!value || "To pole jest wymagane!"
+        "Wymagana liczba całkowita większa od 0.",
+      required: value => !!value || "To pole jest wymagane."
     },
 
-    // form data
-    date: undefined,
+    id: undefined,
+    day: undefined,
     stationaryWorkersLimit: undefined,
     isOpenForCottageWorkers: undefined,
 
-    // calendar stuff
-    value: "",
-
-    today: "2021-03-03",
-    slots: {
-      "2021-03-01": {
-        stationaryWorkersLimit: 20,
-        isOpenForCottageWorkers: true
-      },
-      "2021-03-02": {
-        stationaryWorkersLimit: 20,
-        isOpenForCottageWorkers: false
-      },
-      "2021-03-03": {
-        stationaryWorkersLimit: 10,
-        isOpenForCottageWorkers: false
-      },
-      "2021-03-04": {
-        stationaryWorkersLimit: 0,
-        isOpenForCottageWorkers: true
-      },
-      "2021-03-05": {
-        stationaryWorkersLimit: 20,
-        isOpenForCottageWorkers: true
-      },
-      "2021-03-06": {
-        stationaryWorkersLimit: 20,
-        isOpenForCottageWorkers: true
-      }
-    }
+    value: ""
   }),
   computed: {
-    $_calendar_today() {
-      return dayjs(this.today);
+    today() {
+      return dayjs().format("YYYY-MM-DD");
     },
-    $_calendar_tomorrow() {
-      return this.$_calendar_today
+    tomorrow() {
+      return dayjs()
         .add(dayjs.duration({ days: 1 }))
         .format("YYYY-MM-DD");
     }
   },
-  mounted() {},
+  watch: {
+    dialog(val) {
+      if (!val && this.datePicker) {
+        this.datePicker = false;
+      }
+    }
+  },
+  async mounted() {
+    const { data: slots = [] } = await this.slotList();
+
+    this.slots = slots.reduce(
+      (prev, { id, day, stationaryWorkersLimit, isOpenForCottageWorkers }) =>
+        Object.assign(prev, {
+          [day]: { id, stationaryWorkersLimit, isOpenForCottageWorkers }
+        }),
+      {}
+    );
+  },
   methods: {
-    getStationaryWorkersLimit(date) {
-      return this.slots?.[date]?.stationaryWorkersLimit || "0";
-    },
+    ...mapActions({ slotList: "slotList" }),
     fixStationaryWorkersLimit() {
       const { stationaryWorkersLimit } = this;
 
       this.stationaryWorkersLimit =
         Number.parseInt(stationaryWorkersLimit, 10) || stationaryWorkersLimit;
     },
-    fillSlotForm({ date, future }) {
-      if (future) {
-        this.date = date;
-        this.stationaryWorkersLimit = this.slots?.[
-          date
-        ]?.stationaryWorkersLimit;
-        this.isOpenForCottageWorkers = this.slots?.[
-          date
-        ]?.isOpenForCottageWorkers;
 
-        this.dialog = true;
+    formatDate: date => {
+      const day = dayjs(date);
+      if (day.date() === 1) {
+        return day.format("D MMM");
+      } else {
+        return day.format("D");
       }
+    },
+    getStationaryWorkersLimit(date) {
+      return this.slots?.[date]?.stationaryWorkersLimit || "0";
+    },
+
+    fillSlotForm(date) {
+      const { id, stationaryWorkersLimit, isOpenForCottageWorkers } =
+        this.slots?.[date] || {};
+
+      this.id = id;
+      this.day = date;
+      this.stationaryWorkersLimit = stationaryWorkersLimit;
+      this.isOpenForCottageWorkers = isOpenForCottageWorkers;
+
+      this.dialog = true;
+    },
+    resetSlotForm() {
+      this.$refs.slotForm.reset();
+      this.id = undefined;
     },
     submitSlotForm() {
       if (this.$refs.slotForm.validate()) {
